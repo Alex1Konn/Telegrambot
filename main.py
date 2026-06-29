3f48025e38a3cccc9ff7be07d7d45fc00590163a
@Alex1Konn <n1kolaevna2019@yandex.ru>
2026-06-30 01:24:27 +0400

    Домашнее задание

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 #Создать БД- таблицу (id SERIAL, first_name, last_name, age, phone_number)
 # БОТ на айограме для добавления людей по заданным пользователем
 
-from aiogram import Bot, Dispatcher, types, F
-from aiogram.filters import Command, CommandStart
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command, StateFilter
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import StatesGroup, State
 import psycopg2
+import os
+from dotenv import load_dotenv
+
+# Подключение к базе данных PostgreSQL
+bot = Bot(token=TELEGRAM_API_KEY)
+dp = Dispatcher()
 
 # Подключение к базе данных PostgreSQL
 conn = psycopg2.connect(
@@ -17,41 +22,28 @@ conn = psycopg2.connect(
 )
 cursor = conn.cursor()
 
-import os
-from dotenv import load_dotenv
-import logging
-import aiohttp
-load_dotenv()
-TELEGRAM_API_KEY=os.getenv("TELEGRAM_API_KEY")
-
-class Form(StatesGroup):
-    name = State()
-# Обработчик команды /add для добавления человека
-@dp.message(Сommand("start"))
-async def cmd_star(message:Message, state: FSMContext):
-    await  message.answer(
+#Хендлер  /start
+@dp.message(Command("start"))
+async def cmd_start(message: Message, state: FSMContext):
+    await message.answer(
         "Введите имя, фамилию, возраст и номер телефона через запятую:"
     )
-    await state.set_state(process_add_command)
-    # bot.register_next_step_handler(msg, process_add_command) захват ответа пользователя в телеботе
+    await state.set_state('waiting_for_data')
 
-@dp.message(Command(process_add_command))
+#Хендлер для обработки ответа пользователя
+@dp.message(StateFilter('waiting_for_data'))
+async def process_add_command(message: Message, state: FSMContext):
+    user_input = message.text
 
-"""
-https://ru.stackoverflow.com/questions/1583720/%D1%84%D1%83%D0%BD%D0%BA%D1%86%D0%B8%D1%8F-bot-register-next-step-handler-%D0%B8%D0%B7-telagrambotapi
-"""
-#пример
+def process_add_command(message):
+    try:
+        data = message.text.split(',')
+        first_name = data[0].strip()
+        last_name = data[1].strip()
+        age = int(data[2].strip())
 
-@dp.message(Command())
-async def cmd_start(message:Message):
-    await message.answer(
-        "Привет! Я бот который присылает картинки прямо из популярных сабреддитов!\n"
-        "Используй  команды: \n"
-        "/mem - получить слуйчайную картинку\n"
-        "/help - показать помощь"
-    )
-
-@dp.message(Command("mem"))
-async def cmd_mem(message:Message):
-
-    мппп
+        cursor.execute('INSERT INTO person (first_name, last_name, age) VALUES (%s, %s, %s)', (first_name, last_name, age))
+        conn.commit()
+        bot.send_message(message.chat.id, f'{first_name} {last_name} добавлен в базу данных.')
+    except Exception as e:
+        bot.send_message(message.chat.id, "Ошибка при добавлении человека.")
